60394c05c4b3940fb16be9ccef18bec9e8e12e2c
linqunhui <15096346912@163.com>
2020-08-07 11:27:11 +0800

    1509634691@163.com
    更新jjwt-api版本  优化弃用方法和弃用异常方法

--- console/src/main/java/com/alibaba/nacos/console/utils/JwtTokenUtils.java
+++ console/src/main/java/com/alibaba/nacos/console/utils/JwtTokenUtils.java
@@ -21,9 +21,9 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -47,37 +47,37 @@ import java.util.List;
  */
 @Component
 public class JwtTokenUtils {
-    
+
     private final Logger log = LoggerFactory.getLogger(JwtTokenUtils.class);
-    
+
     private static final String AUTHORITIES_KEY = "auth";
-    
+
     /**
      * minimum SHA_256 secretKey string length.
      */
     private static final int SHA_256_SECRET_CHAR_SIZE = 256 / 8;
-    
+
     /**
      * default SHA_256 secretKey flag.
      */
     private static final String DEFAULT_SECRET_FLAG = "default";
-    
+
     /**
      * custom SHA_256 secretKey from config property.
      */
     @Value("${nacos.security.token.secret-key:default}")
     private String customSecretKeyStr;
-    
+
     /**
      * secret key.
      */
     private SecretKey secretKey;
-    
+
     /**
      * Token validity time(ms).
      */
     private long tokenValidityInMilliseconds;
-    
+
     /**
      * Init.
      */
@@ -103,7 +103,7 @@ public class JwtTokenUtils {
         }
         this.tokenValidityInMilliseconds = 1000 * 60 * 30L;
     }
-    
+
     /**
      * Create token.
      *
@@ -115,11 +115,11 @@ public class JwtTokenUtils {
         long now = System.currentTimeMillis();
 
         Date validity = new Date(now + this.tokenValidityInMilliseconds);
-        
+
         return Jwts.builder().setSubject(authentication.getName()).claim(AUTHORITIES_KEY, "").setExpiration(validity)
                 .signWith(secretKey, SignatureAlgorithm.HS256).compact();
     }
-    
+
     /**
      * Get auth Info.
      *
@@ -128,15 +128,14 @@ public class JwtTokenUtils {
      */
     public Authentication getAuthentication(String token) {
 
-        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-        
+        Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
         List<GrantedAuthority> authorities = AuthorityUtils
                 .commaSeparatedStringToAuthorityList((String) claims.get(AUTHORITIES_KEY));
-        
+
         User principal = new User(claims.getSubject(), "", authorities);
         return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
-    
+
     /**
      * validate token.
      *
@@ -145,9 +144,9 @@ public class JwtTokenUtils {
      */
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
             return true;
-        } catch (SignatureException e) {
+        } catch (SecurityException e) {
             log.info("Invalid JWT signature.");
             log.trace("Invalid JWT signature trace: {}", e);
         } catch (MalformedJwtException e) {
